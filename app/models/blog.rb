e87bc59277007593e06ca085925779e679b8de11
Benjamin Soung <soungbenjamin@gmail.com>
2017-03-04 18:51:37 -0500

    updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+	# 0 is going to be mapped to draft, which is why we set a default in the blog schema to 0
+	enum status: {draft: 0, published: 1}
 	extend FriendlyId
   	friendly_id :title, use: :slugged
 end
