c7374f4e5dc1cb59426481dbc945286b82ab2fba
Benjamin Soung <soungbenjamin@gmail.com>
2017-03-04 16:40:37 -0500

    implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,8 @@
 class PortfoliosController < ApplicationController
+	# before action, goes through and calls set blog method for the following methods inside the brackets
+  	before_action :set_portfolio, only: [:edit, :update]
+
+
 	def index
 		@portfolio_items = Portfolio.all
 	end
@@ -12,17 +16,37 @@ class PortfoliosController < ApplicationController
 	    @portfolio_item = Portfolio.new(portfolio_params)
 
 	    respond_to do |format|
-	      if @portfolio_item.save
-	        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-	        format.json { render :show, status: :created, location: @portfolio_item }
-	      else
-	        format.html { render :new }
-	        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-	      end
+			if @portfolio_item.save
+				format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+				format.json { render :show, status: :created, location: @portfolio_item }
+			else
+				format.html { render :new }
+				format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+			end
     	end
   	end
 
+  	# grab the id
+  	def edit
+  	end
+
+  	def update
+    	respond_to do |format|
+			if @portfolio_item.update(portfolio_params)
+				format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+				format.json { render :show, status: :ok, location: @portfolio_item }
+			else
+				format.html { render :edit }
+				format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+			end
+    	end
+ 	end
+
   	private 
+  		def set_portfolio
+      		@portfolio_item = Portfolio.find(params[:id])
+    	end
+
 	  	def portfolio_params
 	      params.require(:portfolio).permit(:title, :subtitle, :body)
 	    end
