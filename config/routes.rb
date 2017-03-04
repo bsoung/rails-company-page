a57291ba197d86bf864aec61a4ed4a3276f19403
Benjamin Soung <soungbenjamin@gmail.com>
2017-03-04 18:06:15 -0500

    add custom routes override to portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+
+  # ignoring default :show route and passing custom show, and as: adds that prefix we can use for links
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
 
