10ad67bb52942fab4149790948369ce4b2b936cb
Benjamin Soung <soungbenjamin@gmail.com>
2017-03-04 23:06:49 -0500

    implemented image defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,4 +10,14 @@ class Portfolio < ApplicationRecord
 
 	# or using rails and lambda
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
+
+	# callback / set fault after creating - like if main image / thumbnail is not filled
+	after_initialize :set_defaults
+
+	# ||= shorthand for, if self.main_image == nil, then set it to that
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
 end
+
